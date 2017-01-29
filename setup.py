ce54b14aa65d95127fe04f5ae75659741e64f61f
Daniel McMahon <daniel40392@gmail.com>
2017-01-29 21:23:28 +0000

    System Info Package - Testing

---
+++ setup.py
@@ -0,0 +1,19 @@
+'''
+Created on Jan 29, 2017
+
+@author: danie
+'''
+from setuptools import setup
+
+setup(name="systeminfo",
+      version="0.1",
+      description="Basic system information for COMP30670",
+      url="",
+      author="Daniel McMahon",
+      author_email="daniel40392@gmail.com",
+      license="GPL3",
+      packages=['systeminfo'],
+      entry_points={
+          'console_scripts':['comp30670_systeminfo=systeminfo.main:main']
+          }
+      )
---
+++ systeminfo/main.py
@@ -0,0 +1,13 @@
+'''
+Created on Jan 29, 2017
+
+@author: danie
+'''
+import platform
+
+def main():
+    print(platform.platform())
+    return
+
+if __name__ == '__main__':
+    pass
